775cfb4f2cfb7652cad2c7fb0f411eecf62e9964
SELCUK07ERDOGAN <129846212+SELCUK07ERDOGAN@users.noreply.github.com>
2023-04-04 18:50:38 +0300

    Add files via upload

---
+++ Console/Hello/Program.cs
@@ -0,0 +1,9 @@
+﻿int sayi1, sayi2;
+
+Console.Write("1. Sayıyı Giriniz: ");
+sayi1= Convert.ToInt32(Console.ReadLine());
+Console.Write("2. Sayıyı Giriniz: ");
+sayi2= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sayi1+" + "+sayi2+" = "+(sayi1+sayi2));
+Console.WriteLine("{0} + {1} = {2}", sayi1, sayi2, (sayi1+sayi2));
+
